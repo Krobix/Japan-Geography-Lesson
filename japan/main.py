d5a41b8563de510355b2f7ce160102185c1c82ab
Krobix <45689440+Krobix@users.noreply.github.com>
2020-01-18 22:07:18 -0500

    Add files via upload

--- japan/main.py
+++ japan/main.py
@@ -64,22 +64,57 @@ def submit_openended():
 	db.execute("INSERT INTO answers VALUES(?, ?, ?, 0)", (pg, user_id, answer))
 	db.commit()
 	bottle.redirect(f"/pg/{pg+1}")
+
+@bottle.route("/qsubmit/choice")
+def submit_choice():
+	user_id = bottle.request.get_cookie("id")
+	question = int(bottle.request.query.q)
+	answer = bottle.request.query.answer
+	co = 0
+	c = db.cursor()
+	correct = questions.game_questions[question].correct
+	if answer == correct:
+		co = 1
+		c.execute("SELECT * FROM users WHERE id=?", (user_id,))
+		usr = c.fetchone()
+		c.execute("UPDATE users SET score=? WHERE id=?", (int(usr[3]) + 1, usr[0]))
+		db.commit()
+	bottle.redirect(f"/game/{ADMIN_SECRET}/{user_id}?q={question + 1}&c={co}")
 	
 @bottle.route(f"/{ADMIN_SECRET}/<action>")
 def admin_page(action):
+	global game_started
 	if action == "main":
 		return generate_status_page()
+	elif action == "gamestart":
+		game_started = True
+		bottle.redirect(f"/{ADMIN_SECRET}/main")
 		
-@bottle.route(f"/game/{ADMIN_SECRET}/<user_id>", method="POST")
+@bottle.route(f"/game/{ADMIN_SECRET}/<user_id>")
 def game_staring_pg(user_id):
 	real_id = bottle.request.get_cookie("id")
+	question = bottle.request.query.q
+	is_correct = bottle.request.query.c
+	a_msg = ""
+	if is_correct == "":
+		pass
+	else:
+		if is_correct == "0":
+			a_msg = "<script>alert('that was the incorrect answer.')</script>"
+		elif is_correct == "1":
+			a_msg = "<script>alert('that was the correct answer!')</script>"
 	if real_id == None or user_id != real_id:
 		bottle.abort(401, "not logged in!")
 	c = db.cursor()
 	c.execute("UPDATE users SET on_pg=-1 WHERE id=?", (user_id,))
 	db.commit()
 	if game_started:
-		pass
+		if question == "":
+			return questions.game_questions[0].html
+		elif question == "8":
+			bottle.redirect(f"/finished/{user_id}")
+		else:
+			return questions.game_questions[int(question)].html + a_msg
 	else:
 		return bottle.static_file("game_not_started.html", root="./static/")
 		
@@ -89,12 +124,21 @@ def review_pg(user_id, question):
 	real_id = bottle.request.get_cookie("id")
 	if str(real_id) != str(user_id):
 		bottle.abort(401, "Access denied")
-	if question == 6:
-		return get_best_answers(user_id)
+	if question == "5":
+		bottle.redirect(f"/game/{ADMIN_SECRET}/{user_id}")
 	c.execute("SELECT * FROM answers WHERE q_num=? AND NOT user_id=?", (int(question), user_id))
 	answers = c.fetchall()
 	return generate_answer_page(answers, c, int(question), user_id, vote=(int(question) != len(questions.questions)-1))
-	
+
+@bottle.route("/finished/<user_id>")
+def finished_pg(user_id):
+	if user_id != bottle.request.get_cookie("id"):
+		bottle.abort(401, "Access Denied")
+	winners = get_highest_scores()
+	return """
+	<link rel="stylesheet" href="/styles.css"><h1>Here are the winners:</h1><br/><br/>
+	<h1>1st place:</h1><br/><h2>{0}</h2><br/><br/><h1>2nd place:</h1><br/><h2>{1}</h2><br/><br/><h1>3rd place:</h1><br/><h2>{2}</h2>""".format(winners[0][1], winners[1][1], winners[2][1])
+
 @bottle.route("/vote")
 def vote_pg():
 	user_id = bottle.request.get_cookie("id")
@@ -103,11 +147,29 @@ def vote_pg():
 	c = db.cursor()
 	c.execute("SELECT * FROM answers WHERE user_id=? AND q_num=?", (voted_for_id, question))
 	current_votes = c.fetchone()[3]
-	c.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
+	c.execute("SELECT * FROM users WHERE id=?", (user_id,))
 	score = int(c.fetchone()[3])
 	c.execute("UPDATE answers SET votes=? WHERE user_id=? AND q_num=?", (int(current_votes)+1, voted_for_id, question))
-	c.execute("UPDATE users SET score=? WHERE user_id=?", (score += 2, user_id))
+	c.execute("UPDATE users SET score=? WHERE id=?", (score + 2, voted_for_id))
+	db.commit()
 	bottle.redirect(f"/review/{ADMIN_SECRET}/{user_id}/{int(question)+1}")
+
+def get_highest_scores():
+	c = db.cursor()
+	highest = []
+	x = 0
+	while x < 3:
+		c.execute("SELECT * FROM users WHERE score=(SELECT MAX(score) FROM users)")
+		usr = c.fetchone()
+		if usr == None:
+			usr = tuple(" " * 4)
+		else:
+			c.execute("DELETE FROM users WHERE score=?", (usr[3],))
+		highest.append(usr)
+		x += 1
+	for i in highest:
+		c.execute("INSERT INTO users VALUES(?,?,?,?)", i)
+	return highest
 	
 def get_best_answers(user_id):
 	c = db.cursor()
@@ -123,7 +185,7 @@ def generate_status_page():
 	c = db.cursor()
 	c.execute("SELECT * FROM users WHERE name=?", (ADMIN_KEY,))
 	admin_id = c.fetchone()[0]
-	ret_html = f"<link rel='stylesheet' href='/styles.css'>Review link: /review/{ADMIN_SECRET}/{admin_id}/0<br/><br/>"
+	ret_html = f"<link rel='stylesheet' href='/styles.css'>Review link: <a href='/review/{ADMIN_SECRET}/{admin_id}/0'>review</a><br/><br/>"
 	c = db.cursor()
 	c.execute("SELECT * FROM users")
 	all_users = c.fetchall()
@@ -138,12 +200,15 @@ def generate_answer_page(answers, c, q_num, user_id, vote=False):
 	content = f"<link rel='stylesheet' href='/styles.css'><h1>Review: {questions.questions[q_num].prompt}</h1><br/><br/>"
 	for x in answers:
 		c.execute("SELECT * FROM users WHERE id=?", (x[1],))
-		name = c.fetchone()[1]
+		try:
+			name = c.fetchone()[1]
+		except TypeError:
+			name = "[deleted name]"
 		content += f"<div class='answer-review-box'><h2>Name:{name}<br/>ID:{x[1]}</h2><br/><p class='textbook-sect'>Answer:{x[2]}</p></div></br></br>"
 	if vote:
 		content += """
 		<br/><form action="/vote" method="get">
-		<p class="textbook-sect">Copy and paste the long number of the answer that you think is best here.</p>
+		<p class="textbook-sect">Copy and paste the long number of the answer that you think is best here. If there are none or you want to see more, refresh the page after waiting for a few minutes.</p>
 		<br/><input type="hidden" name="question" value="{0}">
 		<input type="text" name="chosen"><br/>
 		<input type="submit" value="Submit" class="b-pretty">
@@ -151,10 +216,10 @@ def generate_answer_page(answers, c, q_num, user_id, vote=False):
 		""".format(q_num)
 	else:
 		content += """
-		<form action="/game/{0}/{1}" method="post">
+		<form action="/game/{0}/{1}" method="get">
 		<input type="submit" class="b-pretty" value="Continue">
 		</form>
 		""".format(ADMIN_SECRET, user_id)
 	return content
 	
-bottle.run(host="localhost", port=80, debug=True)
+bottle.run(host="localhost", port=19275, debug=True)
--- japan/questions.py
+++ japan/questions.py
@@ -10,7 +10,7 @@ class OpenEndedQuestion:
 		<form action="/qsubmit/openended" method="post">
 		<input type="text" class="answer-box" name="answer">
 		<input type="hidden" name="pg" value="{1}">
-		<input type="submit" value="Submit">
+		<input type="submit" class="b-pretty" value="Submit">
 		</form>
 		""".format(self.prompt, self.pg)
 		
@@ -27,12 +27,13 @@ class MultipleChoiceQuestion:
 		<link rel="stylesheet" href="/styles.css">
 		<h1>{0}</h1><br/><br/>
 		<form action="/qsubmit/choice" method="get">
-		<input type="radio" name="answer" value="a">{0}<br/>
-		<input type="radio" name="answer" value="b">{1}<br/>
-		<input type="radio" name="answer" value="c">{2}<br/>
-		<input type="radio" name="answer" value="d">{3}<br/>
+		<input type="hidden" name="q" value="{1}">
+		<input type="radio" name="answer" value="a">{2}<br/>
+		<input type="radio" name="answer" value="b">{3}<br/>
+		<input type="radio" name="answer" value="c">{4}<br/>
+		<input type="radio" name="answer" value="d">{5}<br/>
 		<input type="submit" class="b-pretty" value="Submit">
-		</form>""".format(*self.answers)
+		</form>""".format(self.question, self.id, *self.answers)
 		
 questions = (
 	OpenEndedQuestion(0, "How might the geography of Japan relate to its early conflicts?"),
@@ -43,3 +44,42 @@ questions = (
 	OpenEndedQuestion(5, "Based on the Wikipedia passage, what could have Japan's culture been like?"),
 	OpenEndedQuestion(9999, "The top voted answers for each question, in order.")
 	)
+
+game_questions = (
+	MultipleChoiceQuestion(0,
+	"The four main islands were: Hokkaido, _______, Kyushu, and Shikoku",
+	("Honshu", "Okinawa", "Nara", "Tokyo"),
+	"a"),
+	MultipleChoiceQuestion(1, 
+	"How much of Japan’s land can be farmed?",
+	("20%", "30%", "50%", "15%"),
+	"a"),
+	MultipleChoiceQuestion(2,
+	"How did the Japanese get their food?",
+	("Fishing", "Hunting", "Trading", "None of the Above"),
+	"a"),
+	MultipleChoiceQuestion(3,
+	"Most of Japan is made up of__________",
+	("Islands", "Mountains", "Farmland", "A and B"),
+	"d"),
+	MultipleChoiceQuestion(4,
+	"Why has wildlife in Japan suffered?",
+	("Mountains", "Its Population", "The Land", "None of the Above"),
+	"d"),
+	MultipleChoiceQuestion(5,
+	"A lot of Japan’s mountains are volcanoes",
+	("True", "False", "whats a volcano", "whats a Japan"),
+	"a"),
+	MultipleChoiceQuestion(6,
+	"Mount Fuji is the highest peak in Japan",
+	("True", "False", "aaaaaaaaa", "aaaaaaaa"),
+	"a"),
+	MultipleChoiceQuestion(7,
+	"Japan is not an archipelago",
+	("True", "False", "what", "A R C H I P E L A G O"),
+	"b"),
+	MultipleChoiceQuestion(8,
+	"Early Japanese conflict was caused by a lack of water.",
+	("True", "False", "A R C H I P E L A G O", "O G A L E P I H C R A"),
+	"b")
+)
